02a389b598042360833a79c03e8fafd50812b02e
qq153 <1534433439@qq.com>
2016-11-28 12:39:21 +0800

    3d1128

---
+++ td.js
@@ -0,0 +1,36 @@
+$(function(){
+	//弹出图片
+	
+	var ol=$("#tu ul li").size();
+	var deg = 360/ol;
+	var xdeg = 0,ydeg = -10,xs,ys;
+	//alert(deg);
+	for(var i=ol-1; i>=0; i--){
+		$('#tu ul li').eq(i).css({transition:"1s "+(ol-i)*0.15+
+		"s transform,.5s "+(1+ol*0.15)+"s opacity",'transform':
+		'rotateY('+deg*i+'deg) translateZ(350px)'});
+		}
+		
+		//阻止默认拖动
+		$(document).mousedown(function(e){
+			  e.preventDefault();      //
+			  var x1 = e.clientX;
+			  var y1 = e.clientY;
+			  //alert(x1+'  '+y1);   //显示坐标
+			  $(this).bind('mousemove',function(e){
+				  //拖动坐标
+				  xs = e.clientX - x1;
+				  ys = e.clientY - y1;
+				  x1 = e.clientX;
+			      y1 = e.clientY;
+				  xdeg += xs*0.3;
+				  ydeg -= ys*0.1;
+				  $('#tu').css('transform',
+				  "perspective(800px) rotateX("+ydeg+"deg) rotateY("+xdeg+"deg)");
+				  });
+			}).mouseup(function(){//甩掉鼠标
+				$(this).unbind('mousemove');
+				
+				});
+		
+	});
